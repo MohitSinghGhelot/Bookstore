b0e7fc403c59d050e5bff184ad7c4e71499293e7
MohitSinghGhelot <149100356+MohitSinghGhelot@users.noreply.github.com>
2025-02-13 16:47:01 +0530

    Update Login.jsx

--- Frontend/bookfrontend/app/_components/Login.jsx
+++ Frontend/bookfrontend/app/_components/Login.jsx
@@ -34,7 +34,7 @@ function Login() {
     setErrors({});
 
     try {
-      const response = await fetch("http://localhost:8000/user/login", {
+      const response = await fetch("https://bookstore-backend-kc1u.onrender.com/user/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
